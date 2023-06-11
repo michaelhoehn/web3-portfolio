90085ecbf6ba03afb4ff6c94474141b86f9e21a9
Michael Hoehn <michael.hoehn@burohappold.com>
2023-06-11 00:13:37 -0400

    functional js on hover

--- bgShift.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-const span = document.querySelectorAll("span");
-
-span.forEach((el) => {
-  el.addEventListener("mouseover", () => {
-    let bg = el.getAttribute("data-bg");
-    body.style.background = `url(${bg}) no-repeat center /cover`;
-  });
-});
